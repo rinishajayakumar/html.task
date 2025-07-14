ce4dbb79ed7eb2452d61ed8db95b030d2d5c2caf
RinishaJayakumar <rinishajayakumar@gmail.com>
2025-07-14 13:59:26 +0530

    Add files via upload

---
+++ index.js/index.js
@@ -0,0 +1,17 @@
+var x=3;
+let name='js';
+const iscloudy=true;
+
+console.log(x+" "+name+" "+iscloudy);
+
+document.write(name);
+document.write(x);
+document.write(iscloudy);
+function add(a,b){
+    console.log("this is a function"+(a+b));
+}
+add(4,5);
+let a=parseInt(prompt("enter  the value"));
+let b=parseInt(prompt("enter the second value"));
+ 
+alert(a+b);
