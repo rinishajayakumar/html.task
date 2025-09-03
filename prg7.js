fb658b5490281f93e10f6465ad161565684393ef
RinishaJayakumar <rinishajayakumar@gmail.com>
2025-09-03 09:46:06 +0530

    dom.concept

---
+++ prg7.js
@@ -0,0 +1,5 @@
+//import and export modules
+//definition
+//export allows you to make variables,functions,or classes available to other files
+//import allows you to bring those into another file
+//used in modular javascript(ES6+)
