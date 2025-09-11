becfd16ddd944c916b4b130faafdff682e234c91
RinishaJayakumar <rinishajayakumar@gmail.com>
2025-09-11 09:01:43 +0530

    Delete prg7.js

--- prg7.js
+++
@@ -1,5 +0,0 @@
-//import and export modules
-//definition
-//export allows you to make variables,functions,or classes available to other files
-//import allows you to bring those into another file
-//used in modular javascript(ES6+)
